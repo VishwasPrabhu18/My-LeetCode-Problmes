72468caf672d83a8b25003bc0e4465dbe575551c
Vishwas Prabhu <prabhuvishwas18@gmail.com>
2026-09-23 19:36:57 +0530

    Time: 4 ms (98.07%), Space: 102.3 MB (29.35%) - LeetHub

---
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.java
@@ -0,0 +1,46 @@
+class Solution {
+    public int minOperations(int[] nums, int x) {
+        int n = nums.length;
+
+        int total = 0;
+
+        for (int num : nums) {
+            total += num;
+        }
+
+        int target = total - x;
+
+        // We need to remove the entire array.
+        if (target == 0) {
+            return n;
+        }
+
+        // Impossible to get a negative remaining sum.
+        if (target < 0) {
+            return -1;
+        }
+
+        int left = 0;
+        int sum = 0;
+        int maxLength = -1;
+
+        for (int right = 0; right < n; right++) {
+
+            sum += nums[right];
+
+            while (left <= right && sum > target) {
+                sum -= nums[left];
+                left++;
+            }
+
+            if (sum == target) {
+                maxLength = Math.max(
+                    maxLength,
+                    right - left + 1
+                );
+            }
+        }
+
+        return maxLength == -1 ? -1 : n - maxLength;
+    }
+}
